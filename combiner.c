df36467878ed76dfa67a290adeb5d0d4889a3bed
Aleksandr Rabtsevich <57217014+stavkidisq@users.noreply.github.com>
2022-12-17 15:00:11 +0300

    Update combiner.c

--- combiner.c
+++ combiner.c
@@ -19,10 +19,11 @@ void timeinfo()
 
 int main(int argc, char* argv[])
 {
-    char color[128];
+    char color[1];
     char color_read[256];
     int fd_color_sense = open(argv[1], O_RDONLY);
     int fd_led_blinker = open(argv[2], O_RDONLY);
+    int color_int[3];
 
     while(1)
     {
@@ -32,12 +33,46 @@ int main(int argc, char* argv[])
         printf("from color_sense: %s", color_read);
         fflush(stdout);
 
-        read(fd_led_blinker, color, 256);
+        read(fd_led_blinker, color, 2);
         printf("from led_blinker: %s", color);
         fflush(stdout);
 
         printf("\n");
 
+        char* token = strtok(color_read, ",");
+        sscanf(token, "%d", &color_int[0]);
+        //printf("%d\n", color_int[0]);
+
+        int count = 1;
+        while(token != NULL)
+        {
+            token = strtok(NULL, ",");
+            if(count < 3)
+            {
+                sscanf(token, "%d", &color_int[count]);
+                //printf("%d\n", color_int[count]);
+            }
+            count++;
+        }
+
+
+        if((color[0] == 'R')  && (color_int[0] > color_int[1]) && (color_int[0] > color_int[2]))
+        {
+            printf("red\n");
+        }
+        else if((color[0] == 'G')  && (color_int[1] > color_int[0]) && (color_int[1] > color_int[2]))
+        {
+            printf("green\n");
+        }
+        else if((color[0] == 'Y')  && (color_int[2] > color_int[1]) && (color_int[2] > color_int[0]))
+        {
+            printf("yellow\n");
+        }
+        else
+        {
+            printf("error\n");
+        }
+        
         sleep(2);
     }
 
